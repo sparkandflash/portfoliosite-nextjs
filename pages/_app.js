a0d517dacc09617f5053b1295be1a63de38ea294
praaa <47806016+sparkandflash@users.noreply.github.com>
2021-09-18 12:34:54 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/default.css'
 import '../styles/layout.css'
+import '../styles/media-queries.css'
+import '../styles/magnific-popup.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
